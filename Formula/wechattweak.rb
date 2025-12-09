4699a769436a824e603aaf836f4a39535b921a87
Sunny Young <isunnyyoung@gmail.com>
2025-12-10 00:18:11 +0800

    wechattweak: 2.0.1

--- Formula/wechattweak.rb
+++ Formula/wechattweak.rb
@@ -1,9 +1,9 @@
 class Wechattweak < Formula
-  desc "A command line utility to work with WeChatTweak-macOS."
+  desc "A command-line tool for tweaking WeChat."
   homepage "https://tweaks.app"
-  version "2.0.0"
+  version "2.0.1"
   url "https://github.com/sunnyyoung/WeChatTweak/releases/download/#{version}/wechattweak"
-  sha256 "d26b86778c1d0995797b3635679e75d3d0b822f4bcfbd0fac5744c25b373e6eb"
+  sha256 "2e29571787eaf7044f1d343934cdc1cf939f5edeb73c85285df55f27ce060058"
   license "AGPL-3.0"
 
   depends_on macos: :monterey
